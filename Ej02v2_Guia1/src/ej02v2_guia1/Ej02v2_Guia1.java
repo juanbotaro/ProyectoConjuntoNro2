4e3ee82b6260cd908ce8e5f759d176bbe791f516
juanbotaro <juanbotaro@gmail.com>
2023-08-08 18:41:19 -0300

    Proyecto
    Prueba de proyecto conjunto

---
+++ Ej02v2_Guia1/src/ej02v2_guia1/Ej02v2_Guia1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ej02v2_guia1;
+
+/**
+ *
+ * @author Maquina3
+ */
+public class Ej02v2_Guia1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
